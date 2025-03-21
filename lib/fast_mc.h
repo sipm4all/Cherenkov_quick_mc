b0e63adc3b03d623216ba58f9a4ae42da0ff3414
Nicola Rubini <nicola.rubini@cern.ch>
2025-03-21 15:27:01 +0100

    add fast mc header

---
+++ lib/fast_mc.h
@@ -0,0 +1,11 @@
+//  last update: 21/03/2025
+//  author: Nicola Rubini - nicola.rubini@bo.infn.it
+//  Scope: Cherenkov fast mc
+//  TODO: -
+
+#include "cherenkov.h"
+
+namespace ch_fast_mc
+{
+
+};
