bf56d7a12190ef9e9fd712a1eabe54eb7fc3bb98
Nicola Rubini <nicola.rubini@cern.ch>
2025-03-21 14:46:28 +0100

    Add library of physical constants

---
+++ lib/constants.h
@@ -0,0 +1,8 @@
+//  General physical constants
+const float alhpa_EM = 1. / 137.055;   // [n.u.]
+const float kElectronMass = 0.0005110; // [GeV]
+const float kMuonMass = 0.1057;        // [GeV]
+const float kPionMass = 0.1396;        // [GeV]
+const float kKaonMass = 0.4937;        // [GeV]
+const float kProtonMass = 0.9383;      // [GeV]
+const float kcLight = 299792458.;      // [m/s]
