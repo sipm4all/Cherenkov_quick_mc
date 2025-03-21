61d9d46a4c25a82bea449aeb85679d4e393a8c1f
Nicola Rubini <nicola.rubini@cern.ch>
2025-03-21 15:06:54 +0100

    addition of constants

--- lib/constants.h
+++ lib/constants.h
@@ -1,8 +1,13 @@
 //  General physical constants
-const float alhpa_EM = 1. / 137.055;   // [n.u.]
-const float kElectronMass = 0.0005110; // [GeV]
-const float kMuonMass = 0.1057;        // [GeV]
-const float kPionMass = 0.1396;        // [GeV]
-const float kKaonMass = 0.4937;        // [GeV]
-const float kProtonMass = 0.9383;      // [GeV]
-const float kcLight = 299792458.;      // [m/s]
+const float _c0 = 299792458., speed_of_light = 299792458.;                      // [m / s]
+const float _e = 1.60217663e-19, elementary_charge = 1.60217663e-19;            // [C]
+const float _h = 6.62607015e-34, planck_const = 6.62607015e-34;                 // [m^2 kg / s]
+const float _mu0 = 8.854e-12, vacuum_mag_permittivity = 8.854e-12;              // [F / m]
+const float _a_EM = 1. / 137.035999084, fine_struct_const = 1. / 137.035999084; // [n.u.]
+
+//  Particle masses
+const float kElectronMass = 0.0005110; // [GeV / c^2]
+const float kMuonMass = 0.1057;        // [GeV / c^2]
+const float kPionMass = 0.1396;        // [GeV / c^2]
+const float kKaonMass = 0.4937;        // [GeV / c^2]
+const float kProtonMass = 0.9383;      // [GeV / c^2]
